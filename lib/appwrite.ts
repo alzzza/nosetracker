6fd91169599bf215480de103ee293e3ac0d5ae9b
alzzza <ahdocc78res@gmail.com>
2024-10-31 22:13:41 +0000

    Update appwrite.ts

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,7 +2,7 @@ import { Client, Account, Databases, Storage } from 'appwrite';
 
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('${{ env.PROJECT_ID }}');
+    .setProject('${{ 671e2c0b00385156f79a }}');
 
 export const account = new Account(client);
 export const databases = new Databases(client);
