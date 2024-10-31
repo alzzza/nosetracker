f958d54cf3790db11994121214fe0a3f0864318d
alzzza <ahdocc78res@gmail.com>
2024-10-31 21:06:03 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { Providers } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,11 +16,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
-          <main className="min-h-screen">
-            {children}
-          </main>
-        </Providers>
+        <main className="min-h-screen">
+          {children}
+        </main>
       </body>
     </html>
   )
